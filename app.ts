c2bfa5d634446a985348518f53d49d6f539acb04
boricong <boricong@gmail.com>
2023-03-16 19:47:00 +0900

    3/16 office

--- app.ts
+++ app.ts
@@ -1,27 +1,35 @@
 import express, { Application, Request, Response } from 'express';
+import fs from 'fs';
 import path from 'path';
+import yaml from 'js-yaml'; //typescript에서 js-yaml모듈의 type선언필요 (@types/js-yaml)
 // import HomeController from './src/controllers/HomeController';
 import HomeController from './src/routes/HomeController';
 
 const app: Application = express();
 
+try {
+  const config : any = yaml.load(fs.readFileSync('config.yml', 'utf8'));
 
-// 정적 파일을 제공하기 위한 경로(/public) 등록  (__dirname : 현재 파일 경로)
-app.use(express.static(path.join(__dirname, '/public')));
+  // 정적 파일을 제공하기 위한 경로(/public) 등록  (__dirname : 현재 파일 경로)
+  app.use(express.static(path.join(__dirname, '/public')));
 
-// app.get('/', (req: Request, res: Response) => {
-//   res.send('Hello World!');
-// });
+  // app.get('/', (req: Request, res: Response) => {
+  //   res.send('Hello World!');
+  // });
 
-// GET 요청 처리 (필요에 따라)
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
-});
+  // GET 요청 처리 (필요에 따라)
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+  });
 
-// HomeController를 사용하여 /home 경로로의 GET 요청을 처리합니다.
-app.get('/home', HomeController.index);
+  // HomeController를 사용하여 /home 경로로의 GET 요청을 처리합니다.
+  app.get('/home', HomeController.index);
 
-// 서버 시작
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000!');
-});
+  // 서버 시작
+  app.listen(config.port, () => {
+    console.log('Server is listening on port 3000!');
+  });
+
+  } catch (e) {
+    console.log(e)
+  }
